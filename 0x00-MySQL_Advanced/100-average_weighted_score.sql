e7c149fee007ee512ab7b729ee1aefe3fffb7b17
GideonMissoy <giddykips2030@gmail.com>
2024-06-22 17:56:23 +0300

    13. Average weighted score for all./0-main.py

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,9 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser
+-- computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+END
+$$
+DELIMITER ;

8dde10bd9d7c5611ac4929de5bef3a8f3a2f38f6
GideonMissoy <giddykips2030@gmail.com>
2024-06-22 17:09:03 +0300

    Hot fix.

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
 -- creates a table users
 -- If the table already exists, your script should not fail
+-- script can be executed on any database
 CREATE TABLE If NOT EXISTS `users` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL UNIQUE,
